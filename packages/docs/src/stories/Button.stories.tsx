e69eef5f860aec8426dac3247c57d57717f081be
Lucas Wermann <lw1707@hotmail.com>
2023-01-22 15:01:02 -0300

    Configurando TurboRepo

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,26 @@
+import { Button, ButtonProps } from '@galaxy-ui/react'
+import { StoryObj, Meta } from '@storybook/react'
+
+// configuração global
+export default {
+  title: "Form/Button",
+  component: Button,
+
+  args: {
+    children: 'Enviar',
+  }
+} as Meta<ButtonProps>
+
+
+// variações
+export const Primary: StoryObj<ButtonProps> = {
+  args: {
+    size: 'small',
+  },
+}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  }
+}
